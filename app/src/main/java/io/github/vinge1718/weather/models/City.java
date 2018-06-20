6c748bc7245a8e0df55e7c2f2f341d6cd28d39cc
Vinge1718 <vinge1718@gmail.com>
2018-06-20 21:59:07 +0300

    added the city object to add the population variable to the project

---
+++ app/src/main/java/io/github/vinge1718/weather/models/City.java
@@ -0,0 +1,89 @@
+
+package io.github.vinge1718.weather.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class City {
+
+    @SerializedName("id")
+    @Expose
+    private Long id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("coord")
+    @Expose
+    private Coord coord;
+    @SerializedName("country")
+    @Expose
+    private String country;
+    @SerializedName("population")
+    @Expose
+    private Long population;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public City() {
+    }
+
+    /**
+     * 
+     * @param coord
+     * @param id
+     * @param name
+     * @param population
+     * @param country
+     */
+    public City(Long id, String name, Coord coord, String country, Long population) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.coord = coord;
+        this.country = country;
+        this.population = population;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Coord getCoord() {
+        return coord;
+    }
+
+    public void setCoord(Coord coord) {
+        this.coord = coord;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+
+}
