11d11e3cb3b075b441865db6daac222b706c284d
Vinge1718 <vinge1718@gmail.com>
2018-06-21 11:01:24 +0300

    add the unit conversion parameters in the url

--- app/src/main/java/io/github/vinge1718/weather/Constants.java
+++ app/src/main/java/io/github/vinge1718/weather/Constants.java
@@ -5,6 +5,8 @@ public class Constants {
     public static final String API_KEY = BuildConfig.API_KEY;
     public static final String BASE_URL= "http://api.openweathermap.org/data/2.5";
     public static final String LOCATION_QUERY_PARAMETER = "q";
+    public static final String UNITS = "units";
+    public static final String FORMAT = "metric";
     public static final String API_KEY_PARAM = "appid";
 
 }
