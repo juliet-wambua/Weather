7ccad119253d4a9539fab862d37ff76c1fc89576
Vinge1718 <vinge1718@gmail.com>
2018-06-23 15:23:11 +0300

    add the image files needed to ddisplay the app icons

---
+++ app/src/main/java/io/github/vinge1718/weather/adapters/WeatherPagerAdapter.java
@@ -0,0 +1,4 @@
+package io.github.vinge1718.weather.adapters;
+
+public class WeatherPagerAdapter {
+}
--- app/src/main/java/io/github/vinge1718/weather/ui/WeatherListActivity.java
+++ app/src/main/java/io/github/vinge1718/weather/ui/WeatherListActivity.java
@@ -3,6 +3,8 @@ package io.github.vinge1718.weather.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -16,6 +18,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.github.vinge1718.weather.Constants;
 import io.github.vinge1718.weather.R;
+import io.github.vinge1718.weather.adapters.WeatherListAdapter;
 import io.github.vinge1718.weather.models.Forecast;
 import io.github.vinge1718.weather.models.ForecastList;
 import io.github.vinge1718.weather.services.WeatherService;
@@ -26,8 +29,8 @@ import okhttp3.Response;
 public class WeatherActivity extends AppCompatActivity {
     public List<ForecastList> mWeatherForecasts = new ArrayList<>();
     public static final String TAG = WeatherActivity.class.getSimpleName();
-
-
+    @BindView(R.id.recyclerView) RecyclerView mRecycleView;
+    private WeatherListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +40,6 @@ public class WeatherActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-
-
 
         getWeather(location);
     }
@@ -54,31 +55,16 @@ public class WeatherActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                     final Forecast forecast = weatherService.processResults(response);
+                    //////////////////////////////////////////////////////////////////
                     mWeatherForecasts = forecast.getForecastList();
                     WeatherActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            String[] weatherConditions = new String[mWeatherForecasts.size()];
-                            for(int i = 0; i < weatherConditions.length;i++){
-                                weatherConditions[i] = mWeatherForecasts.get(i).getWeather().get(0).getMain();
-
-
-                                for(ForecastList weatherCondition : mWeatherForecasts){
-                                    Log.d(TAG, "The weather Icon key is: " + Constants.WEATHER_ICON_BASEURL + weatherCondition.getWeather().get(0).getIcon() +Constants.ICON_EXTENSION);
-                                    Log.d(TAG, "Description: " + weatherCondition.getWeather().get(0).getDescription());
-                                    Log.d(TAG, "Temperatures: " + weatherCondition.getMain().getTemp());
-                                    Log.d(TAG, "Maximum Temparatures: " + weatherCondition.getMain().getTempMax());
-                                    Log.d(TAG, "Minimum Temparatures: " + weatherCondition.getMain().getTempMin());
-                                    Log.d(TAG, "Wind Speeds: " +weatherCondition.getWind().getSpeed());
-                                    Log.d(TAG, "Humidity Level: " + weatherCondition.getMain().getHumidity());
-                                    Log.d(TAG, "Atmospherric Pressure: " + weatherCondition.getMain().getPressure());
-                                    Log.d(TAG, "Area Population: " + forecast.getCity().getPopulation());
-                                    //Log.d(TAG, "Time the weather will be in effect: " + weatherCondition.getDtTxt());
-                                    Log.d(TAG, "Latitudinal coordinates: " + forecast.getCity().getCoord().getLat());
-                                    Log.d(TAG, "Longitudinal coordinates: " + forecast.getCity().getCoord().getLon());
-                                    Log.d(TAG, "Formated forecasted time: " + weatherCondition.getDt());
-                                }
-                            }
+                            mAdapter = new WeatherListAdapter(getApplicationContext(), mWeatherForecasts);
+                            mRecycleView.setAdapter(mAdapter);
+                            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(WeatherActivity.this);
+                            mRecycleView.setLayoutManager(layoutManager);
+                            mRecycleView.setHasFixedSize(true);
                         }
                     });
             }
