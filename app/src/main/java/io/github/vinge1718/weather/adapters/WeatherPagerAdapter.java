6d463a0db7ea6fe2dc9ea430c9a29f20a82d3631
Vinge1718 <vinge1718@gmail.com>
2018-06-24 08:22:23 +0300

    redefine the fragment adapter to include the ccityh objecct from Json response

--- app/src/main/java/io/github/vinge1718/weather/adapters/WeatherPagerAdapter.java
+++ app/src/main/java/io/github/vinge1718/weather/adapters/WeatherPagerAdapter.java
@@ -1,4 +1,37 @@
 package io.github.vinge1718.weather.adapters;
 
-public class WeatherPagerAdapter {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+import io.github.vinge1718.weather.models.City;
+import io.github.vinge1718.weather.models.ForecastList;
+import io.github.vinge1718.weather.ui.WeatherDetailFragment;
+
+public class WeatherPagerAdapter extends FragmentPagerAdapter{
+    private List<ForecastList> mWeatherForecasts;
+    private City mCity;
+
+    public WeatherPagerAdapter(FragmentManager fm, List<ForecastList> weatherForecast, City city){
+        super(fm);
+        mWeatherForecasts = weatherForecast;
+        mCity = city;
+    }
+
+    @Override
+    public Fragment getItem(int position){
+        return WeatherDetailFragment.newInstance(mWeatherForecasts.get(position), mCity);
+    }
+
+    @Override
+    public int getCount(){
+        return mWeatherForecasts.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        return mWeatherForecasts.get(position).getReadableDate();
+    }
 }
