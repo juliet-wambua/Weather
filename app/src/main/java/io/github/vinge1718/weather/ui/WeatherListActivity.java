fdbf475887fcd9e80eb15fd4ab2f3605331282cc
Vinge1718 <vinge1718@gmail.com>
2018-07-02 14:17:33 +0300

    add the summarised implementation of the weather object display object

--- app/src/main/java/io/github/vinge1718/weather/ui/WeatherListActivity.java
+++ app/src/main/java/io/github/vinge1718/weather/ui/WeatherListActivity.java
@@ -1,10 +1,13 @@
 package io.github.vinge1718.weather.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,6 +16,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.github.vinge1718.weather.Constants;
 import io.github.vinge1718.weather.R;
 import io.github.vinge1718.weather.adapters.WeatherListAdapter;
 import io.github.vinge1718.weather.models.Forecast;
@@ -23,6 +27,10 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherListActivity extends AppCompatActivity {
+
+//    public String mRecentAddress;
+//    public SharedPreferences mSharedPreferences;
+
     public List<ForecastList> mWeatherForecasts = new ArrayList<>();
     public static final String TAG = WeatherListActivity.class.getSimpleName();
     @BindView(R.id.recyclerView) RecyclerView mRecycleView;
@@ -36,8 +44,13 @@ public class WeatherListActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-
         getWeather(location);
+
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
+//        if(mRecentAddress != null){
+//            getWeather(mRecentAddress);
+//        }
     }
 
     private void getWeather(String location){
