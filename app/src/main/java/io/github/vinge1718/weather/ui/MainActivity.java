54e92f73c8c6fea4348ef5c57290c242d0c91abc
Vinge1718 <vinge1718@gmail.com>
2018-07-02 14:10:05 +0300

    added the authorisation code for login and logout functions

--- app/src/main/java/io/github/vinge1718/weather/ui/MainActivity.java
+++ app/src/main/java/io/github/vinge1718/weather/ui/MainActivity.java
@@ -1,25 +1,69 @@
 package io.github.vinge1718.weather.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.github.vinge1718.weather.Constants;
 import io.github.vinge1718.weather.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+//    private SharedPreferences mSharedPreferences;
+//    private SharedPreferences.Editor mEditor;
+    private DatabaseReference mSearchedLocationReference;
+    private ValueEventListener mSearchLocationReferenceListener;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private static final int RC_SIGN_IN = 1;
+
     @BindView(R.id.locationEditText) EditText mLocationEditText;
     @BindView(R.id.appName) TextView mAppName;
     @BindView(R.id.checkForeCast) Button mCheckForecast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mSearchedLocationReference = FirebaseDatabase.getInstance().getReference().child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
+        mSearchLocationReferenceListener = mSearchedLocationReference.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                for(DataSnapshot locationSnapshot: dataSnapshot.getChildren()){
+                    String location = locationSnapshot.getValue().toString();
+                    Log.d("Saved Location", "locations: " + location);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError){}
+        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
@@ -27,7 +71,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Typeface dancingScriptFont = Typeface.createFromAsset(getAssets(), "fonts/DancingScript-Regular.otf");
         mAppName.setTypeface(dancingScriptFont);
 
+
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mEditor = mSharedPreferences.edit();
+
         mCheckForecast.setOnClickListener(this);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Toast.makeText(MainActivity.this, "You're now signed in. Welcome to the Weather App.", Toast.LENGTH_SHORT).show();
+                }else{
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mSearchedLocationReference.removeEventListener(mSearchLocationReferenceListener);
     }
 
     @Override
@@ -35,8 +109,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(v == mCheckForecast){
             Intent intent = new Intent(MainActivity.this, WeatherListActivity.class);
             String mLocation = mLocationEditText.getText().toString();
+            saveLocationToFireBase(mLocation);
+//            if(!(mLocation=="")){
+//                addToSharedPreferences(mLocation);
+//            }
             intent.putExtra("location", mLocation);
             startActivity(intent);
         }
     }
+
+    public void saveLocationToFireBase(String location){
+        mSearchedLocationReference.push().setValue(location);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                //sign out
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // Sign-in succeeded, set up the UI
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // Sign in was canceled by the user, finish the activity
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+//    private void addToSharedPreferences(String location) {
+//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+//    }
 }
