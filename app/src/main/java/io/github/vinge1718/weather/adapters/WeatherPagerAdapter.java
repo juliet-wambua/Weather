09d4581054778df9fbeb447b6b36aceabe45396d
Vinge1718 <vinge1718@gmail.com>
2018-07-02 14:43:05 +0300

    add adapter to populate the list view of the weather forecast items

--- app/src/main/java/io/github/vinge1718/weather/adapters/WeatherPagerAdapter.java
+++ app/src/main/java/io/github/vinge1718/weather/adapters/WeatherPagerAdapter.java
@@ -7,22 +7,25 @@ import android.support.v4.app.FragmentPagerAdapter;
 import java.util.List;
 
 import io.github.vinge1718.weather.models.City;
+import io.github.vinge1718.weather.models.Forecast;
 import io.github.vinge1718.weather.models.ForecastList;
 import io.github.vinge1718.weather.ui.WeatherDetailFragment;
 
 public class WeatherPagerAdapter extends FragmentPagerAdapter{
     private List<ForecastList> mWeatherForecasts;
     private City mCity;
+    private Forecast mForecast;
 
-    public WeatherPagerAdapter(FragmentManager fm, List<ForecastList> weatherForecast, City city){
+    public WeatherPagerAdapter(FragmentManager fm, List<ForecastList> weatherForecast, City city, Forecast forecast){
         super(fm);
         mWeatherForecasts = weatherForecast;
         mCity = city;
+        mForecast = forecast;
     }
 
     @Override
     public Fragment getItem(int position){
-        return WeatherDetailFragment.newInstance(mWeatherForecasts.get(position), mCity);
+        return WeatherDetailFragment.newInstance(mWeatherForecasts.get(position), mCity, mForecast);
     }
 
     @Override
